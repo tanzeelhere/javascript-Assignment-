5ec38ef3cda86d232d2137da53601053f448d5aa
tanzeelhere <100508865+tanzeelhere@users.noreply.github.com>
2022-10-14 09:21:12 +0500

    Add files via upload

---
+++ Exer_1.js
@@ -0,0 +1,8 @@
+
+ /////////////////////////////Answer one/////////////////////////////
+
+
+ document.write("Answer No one");
+
+ let myname = "Tanzeel ur Rehman ";
+ document.write("<br>", myname);
